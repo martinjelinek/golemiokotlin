591077fa607757aabab0e2dc267e6a1d2033b130
Martin Jelínek <martin.jelinek1@gendigital.com>
2024-02-28 02:58:02 +0100

    add playgrounds client

---
+++ golemiokotlinlib/src/commonMain/kotlin/cz/vse/golemiokotlinlib/v2/client/PlaygroundsClient.kt
@@ -0,0 +1,34 @@
+package cz.vse.golemiokotlinlib.v2.client
+
+import cz.vse.golemiokotlinlib.v2.service.IPlaygroundsRepository
+import cz.vse.golemiokotlinlib.v2.service.impl.cache.PlaygroundsCachingRepository
+
+/**
+ * Exposed client class providing bicycle counters data requests.
+ */
+class PlaygroundsClient(apiKey: String) {
+
+    private val repository: IPlaygroundsRepository = PlaygroundsCachingRepository.create(apiKey)
+
+    /**
+     * TODO doc
+     */
+    suspend fun getAllPlaygrounds(
+        latlng: Pair<String, String>?,
+        range: Int?,
+        districts: List<String>?,
+        limit: Int?,
+        offset: Int?,
+        updatedSince: String?
+    ) = repository.getAllPlaygrounds(latlng, range, districts, limit, offset, updatedSince)
+
+    /**
+     * TODO doc
+     */
+    suspend fun getPlaygroundById(id: String) = repository.getPlaygroundById(id)
+
+    /**
+     * TODO doc
+     */
+    suspend fun getPlaygroundsProperties() = repository.getPlaygroundsProperties()
+}
