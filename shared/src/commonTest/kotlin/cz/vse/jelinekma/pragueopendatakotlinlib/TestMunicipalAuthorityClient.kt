aca2133e56ad45564fe0733b1031e0a5eca0047b
Martin Jelínek <martin.jelinek1@gendigital.com>
2024-02-29 03:08:54 +0100

    add authorities client test

--- shared/src/commonTest/kotlin/cz/vse/jelinekma/pragueopendatakotlinlib/TestMunicipalAuthorityClient.kt
+++ shared/src/commonTest/kotlin/cz/vse/jelinekma/pragueopendatakotlinlib/TestMunicipalAuthorityClient.kt
@@ -4,6 +4,8 @@ import cz.vse.golemiokotlinlib.v2.client.MunicipalAuthoritiesClient
 import kotlinx.coroutines.test.runTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  * Tests municipal authorities client.
@@ -19,14 +21,32 @@ class TestMunicipalAuthorityClient : TestClient() {
 
     @Test
     fun testGetAllMunicipalAuthorities() = runTest {
+        val testData = client.getAllMunicipalAuthorities(
+            latlng,
+            50000,
+            null,
+            null,
+            limit,
+            offset,
+            updatedSince
+        )
 
+        assertTrue { testData.isNotEmpty() }
     }
 
     @Test
     fun getMunicipalAuthorityById() = runTest {
-        // todo zminit ze v golemiu nabizi blby id na testovani
-        // todo spatny ID vraci chybu - podchytit, mozna staci jen vratit null - content type je totiz null
-        val data = client.getMunicipalAuthorityById("urad-mestske-casti-praha-6")
+        val id = "magistrat-hlavniho-mesta-prahy"
+        val data = client.getMunicipalAuthorityById(id)
+
+        assertEquals(data.properties?.id, id)
     }
-    // endregion
+
+    @Test
+    fun getMunicipalAuthoritiesQueues() = runTest {
+        val id = "skoduv-palac"
+        val testData = client.getMunicipalAuthoritiesQueues(id)
+
+        assertEquals(testData.properties?.id, id)
+    }
 }
