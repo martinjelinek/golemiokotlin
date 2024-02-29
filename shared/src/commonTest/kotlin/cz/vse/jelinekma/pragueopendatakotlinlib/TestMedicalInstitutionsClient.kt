fe5c5bd9d811840a487e7c645d2e7769d297610e
Martin Jelínek <martin.jelinek1@gendigital.com>
2024-02-29 02:45:48 +0100

    adjust medical client test

--- shared/src/commonTest/kotlin/cz/vse/jelinekma/pragueopendatakotlinlib/TestMedicalInstitutionsClient.kt
+++ shared/src/commonTest/kotlin/cz/vse/jelinekma/pragueopendatakotlinlib/TestMedicalInstitutionsClient.kt
@@ -2,6 +2,7 @@ package cz.vse.jelinekma.pragueopendatakotlinlib
 
 import cz.vse.golemiokotlinlib.v2.client.MedicalInstitutionsClient
 import cz.vse.golemiokotlinlib.v2.entity.responsedata.MedicalGroup
+import cz.vse.golemiokotlinlib.v2.entity.responsedata.MedicalInstitutionTypes
 import kotlinx.coroutines.test.runTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -40,5 +41,41 @@ class TestMedicalInstitutionsClient : TestClient() {
             testData.properties?.name,
             "Dopravní podnik hl. m. Prahy a.s., Praktický lékař a rehabilitace"
         )
+    }
+
+    @Test
+    fun getMedicalInstitutionsTypesTest() = runTest {
+        val testData = client.getMedicalInstitutionTypes()
+
+        val expectedDate = MedicalInstitutionTypes(
+            listOf(
+                "Nemocniční lékárna s odbornými pracovišti",
+                "Lékárna",
+                "Lékárna s odbornými pracovišti",
+                "Výdejna",
+                "Vojenská lékárna",
+                "Vojenská lékárna s OOVL",
+                "Nemocniční lékárna",
+                "Lékárna s odbornými pracovišti s OOVL",
+                "Nemocniční lékárna s OOVL",
+                "Nemocniční lékárna s odbor. pracovišti s OOVL",
+                "Odloučené oddělení výdeje léčivých přípravků",
+                "Lékárna s OOVL",
+                "Lékárna s odborným pracovištěm, která zásobuje lůžková zdravotnická zařízení",
+                "Lékárna s odborným pracovištěm, která zásobuje lůžková zdravotnická zařízení s OOVL",
+                "Lékárna zásobujicí zařízení ústavní péče",
+                "Lékárna zásobujicí zařízení ústavní péče s OOVL"
+            ),
+            listOf(
+                "Fakultní nemocnice",
+                "Nemocnice",
+                "Ostatní ambulantní zařízení",
+                "Ostatní zdravotnická zařízení",
+                "Zdravotní záchranná služba",
+                "Zdravotnické středisko"
+            ),
+        )
+
+        assertEquals(testData, expectedDate)
     }
 }
